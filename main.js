2c36c7d876bcdfdf1c02f01436b00d534aa60498
guels <miguelramos20100203@gmail.com>
2026-05-06 11:10:40 -0400

    começo de tudo
    comecei o código mas sem muita evolução; instalei o prompt e instalei o readline-sync

---
+++ main.js
@@ -0,0 +1,14 @@
+const prompt = require('prompt-sync');
+const readline = require('readline-sync');
+const livro = require('./Livro');
+let opcao;
+
+//const libro = [];
+
+while(true){
+  opcao = readline.quetsion('----BIBLIOTECA---- \n 1 - Cadastrar livro \n 2 - Listar livros \n 3 - Alterar livro \n 4 - Estatisticas \n 0 - Sair');
+      
+  
+
+
+}
